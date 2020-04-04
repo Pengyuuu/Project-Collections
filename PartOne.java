685e6ae97fee9189f1b9cc3dfbbf82b48abaa41b
Eric Truong <pengu@erics-mbp-2.lan>
2020-04-03 21:14:37 -0700

    Working on reading the file

--- PartOne.java
+++ PartOne.java
@@ -1,9 +1,33 @@
-import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Collections;
 
 public class PartOne {
 
     public static void main (String[] args){
 
-        
+
+    }
+
+    public static void readFile() {
+
+        try {
+
+            Scanner read = new Scanner(new File("file.txt"));
+
+            do {
+
+                String line = read.nextLine();
+            }
+
+            while (read.hasNext());
+
+            read.close();
+        }
+
+        catch (FileNotFoundException fnf){
+
+            System.out.println("File not found");
+        }
     }
 }
