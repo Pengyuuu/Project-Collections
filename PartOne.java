23f36f0d9dda0dadfcc581f45abb0cb21e0a1d51
Eric Truong <pengu@erics-mbp-2.lan>
2020-04-05 15:09:19 -0700

    Working on the maps

--- PartOne.java
+++ PartOne.java
@@ -1,23 +1,71 @@
 import java.io.*;
 import java.util.Scanner;
-import java.util.Collections;
+import java.util.*;
 
 public class PartOne {
 
     public static void main (String[] args){
 
+        Scanner scan = new Scanner(System.in);
 
+        System.out.print("1. Map with just the word \n" +
+                "2. Map with scrabble point value \n" +
+                "Make a selection: ");
+
+        int choiceOne = scan.nextInt();
+
+        if (choiceOne == 1){
+
+            System.out.print("1. TreeMap \n" +
+                    "2. HashMap \n" +
+                    "Make a selection: ");
+
+            int choiceTwo = scan.nextInt();
+
+            if (choiceTwo == 1){
+
+                long startTime = System.nanoTime();
+
+                readToTree();
+
+                long estimatedTime = System.nanoTime() - startTime;
+
+                System.out.print("Amount of time: " + estimatedTime);
+            }
+        }
+
+        else {
+
+            System.out.print("1. TreeMap \n" +
+                    "2. HashMap \n" +
+                    "Make a selection: ");
+
+            int choiceTwo = scan.nextInt();
+
+            if (choiceTwo == 1){
+
+
+            }
+        }
     }
 
-    public static void readFile() {
+    public static TreeMap readToTree() {
+
+        TreeMap<String, Integer> tree = new TreeMap<>();
 
         try {
 
             Scanner read = new Scanner(new File("file.txt"));
 
+            int i = 0;
+
             do {
 
                 String line = read.nextLine();
+
+                tree.put(line, i);
+
+                i++;
             }
 
             while (read.hasNext());
@@ -29,5 +77,12 @@ public class PartOne {
 
             System.out.println("File not found");
         }
+
+        return tree;
+    }
+
+    public static HashMap readToHash() {
+
+        HashMap<String, Integer>
     }
 }
