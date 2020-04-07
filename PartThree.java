1cdca69e058ec12f9d405983da0622e85c389d66
erinalara <erinalara888@gmail.com>
2020-04-07 04:51:54 -0700

    hi

--- PartThree.java
+++ PartThree.java
@@ -1,4 +1,35 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.*;
+import java.util.ArrayList;
+
+
 public class PartThree {
+
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("1. ArrayList \n" +
+                "2. LinkedList \n" +
+                "Make a selection: ");
+
+        int choiceOne = scan.nextInt();
+
+        if (choiceOne == 1) {
+            ArrayList<String> list = new ArrayList<String>();
+
+
+        }
+
+        else {
+            LinkedList<String> list = new LinkedList<String>();
+        }
+
+
+    }
+
 
 
 
