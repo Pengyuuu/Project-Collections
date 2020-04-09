441a1b4535d5b769fb29389c93c578cb91604351
erinalara <erinalara888@gmail.com>
2020-04-08 17:34:24 -0700

    Working on 3

--- PartThree.java
+++ PartThree.java
@@ -19,9 +19,12 @@ public class PartThree {
         System.out.println("How many teams? ");
         int teams = scan.nextInt();
 
+        System.out.println("What element do you want to work with. Enter a number between 1 and 100.");
+        int element = scan.nextInt();
+
 
         if (choiceOne == 1) {
-            ArrayList<String> list = new ArrayList<String>();
+           ArrayList<String> list = loadArrayList();
 
 
         }
@@ -33,6 +36,71 @@ public class PartThree {
 
     }
 
+
+    public static ArrayList loadArrayList() {
+        long startTime = System.nanoTime();
+
+        ArrayList<String> list = new ArrayList<String>();
+
+        try {
+
+            Scanner read = new Scanner(new File("items.txt"));
+
+            do {
+
+                String line = read.nextLine();
+                list.add(line);
+
+            }
+
+            while (read.hasNext());
+            read.close();
+        }
+
+        catch (FileNotFoundException fnf){
+
+            System.out.println("File not found");
+        }
+
+
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.print("Amount of time to load to Arraylist: " + estimatedTime);
+
+        return list;
+
+    }
+
+    public static LinkedList loadLinked() {
+        long startTime = System.nanoTime();
+
+        LinkedList<String> list = new LinkedList<>();
+
+        try {
+
+            Scanner read = new Scanner(new File("items.txt"));
+
+            do {
+
+                String line = read.nextLine();
+                list.add(line);
+
+            }
+
+            while (read.hasNext());
+            read.close();
+        }
+
+        catch (FileNotFoundException fnf){
+
+            System.out.println("File not found");
+        }
+
+        long estimatedTime = System.nanoTime() - startTime;
+        System.out.print("Amount of time to load to Linked List: " + estimatedTime);
+
+        return list;
+
+    }
 
 
 
