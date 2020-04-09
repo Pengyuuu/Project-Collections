23cd121b0a6c9c01bbf8a7f9913d3c81a91d6c61
Eric Truong <pengu@erics-mbp-2.lan>
2020-04-08 22:40:09 -0700

    Finished part one

--- PartOne.java
+++ PartOne.java
@@ -1,3 +1,11 @@
+/**@authors Erina Lara, Eric Truong
+ *Date: April 8, 2020
+ *Program: Reads in a text file full of words into either a tree map or hash map, depending on user choice. Also reads
+ *in another text file full of letters with their scrabble point values into either a tree map or hash map. Time how
+ *long either map would take to read in the words, find which ever is fastest. Time both to see which is fastest in
+ *searching for a value.
+ */
+
 import java.io.*;
 import java.util.Scanner;
 import java.util.*;
@@ -18,36 +26,69 @@ public class PartOne {
 
             TreeMap<String, Integer> scrabbleTree = scrabbleToTree();
 
+            TreeMap<String, Integer> textTree = new TreeMap<>();
+
             long startTime = System.nanoTime();
 
-            readToTree(scrabbleTree);
+            textTree = readToTree();
 
             long estimatedTime = System.nanoTime() - startTime;
 
-            System.out.print("Amount of time: " + estimatedTime);
+            System.out.println("Amount of time to load: " + estimatedTime);
+
+            startTime = System.nanoTime();
+
+            textTree = searchTree(textTree, scrabbleTree);
+
+            for (String key : textTree.keySet()) {
+
+                System.out.println(key + " -> " + textTree.get(key));
+            }
+
+            estimatedTime = System.nanoTime() - startTime;
+
+            System.out.println("Amount of time to search: " + estimatedTime);
+
         }
 
         else {
 
             HashMap<String, Integer> scrabbleHash = scrabbleToHash();
 
+            HashMap<String, Integer> textHash = new HashMap<>();
+
             long startTime = System.nanoTime();
 
-            readToHash(scrabbleHash);
+            textHash = readToHash();
 
             long estimatedTime = System.nanoTime() - startTime;
 
-            System.out.print("Amount of time: " + estimatedTime);
+            System.out.println("Amount of time to load: " + estimatedTime);
 
+            startTime = System.nanoTime();
+
+            textHash = searchHash(textHash, scrabbleHash);
+
+            for (String key : textHash.keySet()) {
+
+                System.out.println(key + " -> " + textHash.get(key));
+            }
+
+            estimatedTime = System.nanoTime() - startTime;
+
+            System.out.println("Amount of time to search: " + estimatedTime);
         }
 
     }
 
-    public static TreeMap readToTree(TreeMap scrabbleTree) {
+    /**
+     * Reads words in text file into a tree map
+     * @return  tree map with words from text file
+     */
+    public static TreeMap readToTree() {
 
         TreeMap<String, Integer> tree = new TreeMap<>();
 
-        int x = 0;
         try {
 
             Scanner read = new Scanner(new File("file.txt"));
@@ -55,8 +96,8 @@ public class PartOne {
             do {
 
                 String line = read.nextLine();
-                x = findValueInTree(scrabbleTree, line);
-                tree.put(line,x);
+                //x = findValueInTree(scrabbleTree, line);
+                tree.put(line, 0);
 
             }
 
@@ -73,18 +114,21 @@ public class PartOne {
         return tree;
     }
 
-    public static HashMap readToHash(HashMap scrabbleHash) {
+    /**
+     * Reads words from text file into a hash map
+     * @return  hash map with words from text file
+     */
+    public static HashMap readToHash() {
 
         HashMap<String, Integer> tree = new HashMap<>();
-        int x = 0;
 
         try {
             Scanner read = new Scanner(new File("file.txt"));
 
             do {
                 String line = read.nextLine();
-                x = findValueInHash(scrabbleHash, line);
-                tree.put(line,x);
+                //x = findValueInHash(scrabbleHash, line);
+                tree.put(line, 0);
             }
 
             while (read.hasNext());
@@ -99,6 +143,10 @@ public class PartOne {
         return tree;
     }
 
+    /**
+     * Reads letters with their scrabble point values into a tree map
+     * @return  tree map with letters and their scrabble point values
+     */
     public static TreeMap scrabbleToTree() {
 
         TreeMap<String, Integer> scrabbleTree = new TreeMap<>();
@@ -131,6 +179,10 @@ public class PartOne {
         return scrabbleTree;
     }
 
+    /**
+     * Reads letters and their scrabble point values from a text file into a hash map
+     * @return  hash map with letters and their scrabble point values
+     */
     public static HashMap scrabbleToHash() {
 
         HashMap<String, Integer> scrabbleHash = new HashMap<>();
@@ -163,6 +215,12 @@ public class PartOne {
         return scrabbleHash;
     }
 
+    /**
+     * Calculates the value of a word
+     * @param scanTree  tree containing point value of letters
+     * @param n         word whose value is being calculated
+     * @return          point value of the word
+     */
     public static int findValueInTree(TreeMap scanTree, String n) {
 
         int total = 0;
@@ -174,7 +232,6 @@ public class PartOne {
                 total  += (int) scanTree.get( Character.toString(letter).toUpperCase());
 
             }
-
         }
 
         catch (NullPointerException nullP) {
@@ -184,6 +241,12 @@ public class PartOne {
         return total;
     }
 
+    /**
+     * Calculates the value of a word
+     * @param scanHash  hash map containing point value of letters
+     * @param n         word whose value is being calculated
+     * @return          point value of the word
+     */
     public static int findValueInHash(HashMap scanHash, String n) {
 
         int total = 0;
@@ -197,7 +260,6 @@ public class PartOne {
                 total  += (int) scanHash.get( Character.toString(letter).toUpperCase());
 
             }
-
         }
 
         catch (NullPointerException nullP) {
@@ -206,4 +268,50 @@ public class PartOne {
 
         return total;
     }
+
+    /**
+     * Modifies value of words in a tree map
+     * @param textTree      tree map to be modified
+     * @param scrabbleTree  tree map with letters and their point values
+     * @return              modified tree map
+     */
+    public static TreeMap searchTree (TreeMap textTree, TreeMap scrabbleTree){
+
+        int value = 0;
+
+        TreeMap<String, Integer> tree = new TreeMap<>(textTree);
+
+        for (String key : tree.keySet()) {
+
+            value = findValueInTree(scrabbleTree, key);
+
+            textTree.put(key, value);
+        }
+        return textTree;
+    }
+
+    /**
+     * Modifies value of words in a hash map
+     * @param textHash      hash map to be modified
+     * @param scrabbleHash  hash map with letters and their point values
+     * @return              modified hash map
+     */
+    public static HashMap searchHash (HashMap textHash, HashMap scrabbleHash) {
+
+        int value = 0;
+
+        HashMap<String, Integer> hash = new HashMap<>(textHash);
+
+        for (String key : hash.keySet()) {
+
+            value = findValueInHash(scrabbleHash, key);
+
+            textHash.put(key, value);
+
+        }
+
+        return textHash;
+    }
 }
+
+
